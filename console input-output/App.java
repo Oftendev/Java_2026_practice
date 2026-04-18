76dbc9c604a643e75d2cec459a5f1e628f1306b5
Oftendev <karim.m.2004@gmail.com>
2026-04-18 23:17:17 +0300

    Сделал практику

---
+++ console input-output/App.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.nio.charset.Charset;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in, Charset.forName("CP866")); //В моём случае ввод идёт в кодировке CP866 для кириллицы
+        System.out.print("Введите имя: ");
+        String name = scan.next();
+        System.out.println("Привет, " + name);
+        scan.close();
+    }
+}
